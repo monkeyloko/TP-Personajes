4c00fc57891c7fcc419e3fec6b7db4cd00beb141
SANTIAGO DOFF <46873259@ort>
2023-07-14 12:08:56 -0300

    agrego swagger

--- Server.js
+++ Server.js
@@ -6,6 +6,8 @@ import AuthRouter from "./src/Controller/authController.js";
 import PeliserieRouter from "./src/Controller/peliserieController.js";
 import passport from "passport";
 import { jwtStrategy } from './src/Common/jwt.strategy.js';
+import swaggerDoc from './openapi.json' assert { type: "json" };
+import swaggerUI from "swagger-ui-express"
 
 const app = express();
 const port = 5000;
@@ -20,7 +22,11 @@ app.use("/auth", AuthRouter);
 app.use("/characters", PersonajeRouter);
 app.use("/movies", PeliserieRouter);
 
-
+app.use('/api-docs', swaggerUI.serve);
+app.get(
+  '/api-docs',
+   swaggerUI.setup(swaggerDoc)
+);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
